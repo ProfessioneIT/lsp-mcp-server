3ebc934fbc47b2d5335bcd2cf568fd1a6a13e391
Ivan Iraci <ivan.iraci@professioneit.com>
2026-01-23 13:21:06 +0100

    chore: Add copyright headers to all source files

--- src/utils/uri.ts
+++ src/utils/uri.ts
@@ -1,3 +1,25 @@
+/**
+ * Copyright (c) 2026 Ivan Iraci <ivan.iraci@professioneit.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { BINARY_EXTENSIONS, BINARY_CHECK_BYTES } from '../constants.js';
@@ -216,7 +238,7 @@ export async function readFile(filePath: string): Promise<string> {
 
   try {
     return await fs.promises.readFile(filePath, 'utf-8');
-  } catch (error) {
+  } catch (_error) {
     throw new LSPError(
       LSPErrorCode.FILE_NOT_READABLE,
       `Cannot read file: ${filePath}`,
